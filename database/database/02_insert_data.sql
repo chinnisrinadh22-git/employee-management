b7572da1fef764196c70097ae204e0347c85a2e7
chinnisrinadh22-git <chinnisrinadh22@gmail.com>
2026-07-03 08:32:21 +0530

    Create 02_insert_data.sql

---
+++ database/database/02_insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary)
+VALUES
+(101, 'Rahul', 'IT', 50000),
+(102, 'Priya', 'HR', 45000),
+(103, 'Arjun', 'Finance', 60000),
+(104, 'Sneha', 'IT', 55000),
+(105, 'Kiran', 'Sales', 48000);
